2c80be147da7ac9374cf1911fdc80bf10455ac48
Krithic Kumar <krithickumarub@protonmail.com>
2024-03-19 22:58:33 +0530

    add release flag to shell script

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-cmake -B build && cmake --build build
+CMAKE_FLAGS=""
+while [[ $# -gt 0 ]]; do
+	key="$1"
+
+	case $key in
+	--release)
+		CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Release"
+		shift
+		;;
+	*)
+		echo "Usage: $0 [--release]" >&2
+		exit 1
+		;;
+	esac
+done
+
+cmake $CMAKE_FLAGS -B build && cmake --build build
--- build_web.sh
+++ build_web.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-cmake -DCMAKE_TOOLCHAIN_FILE=/usr/lib/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM_WEB=ON -B build &&
+CMAKE_FLAGS=""
+while [[ $# -gt 0 ]]; do
+	key="$1"
+
+	case $key in
+	--release)
+		CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Release"
+		shift
+		;;
+	*)
+		echo "Usage: $0 [--release]" >&2
+		exit 1
+		;;
+	esac
+done
+
+cmake -DCMAKE_TOOLCHAIN_FILE=/usr/lib/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM_WEB=ON $CMAKE_FLAGS -B build &&
 	cmake --build build
