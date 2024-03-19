2a047cb7907f10ecdc7c473f72891acb426c921a
Krithic Kumar <krithickumarub@protonmail.com>
2024-03-19 22:07:13 +0530

    proper CMakeLists config for both web and desktop
    simple text window working on both targets

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stddef.h>
+#include <stdio.h>
 
 #include "raylib/raylib.h"
+
+#if defined(PLATFORM_WEB)
+#include "emscripten/emscripten.h"
+#endif /* ifdef PLATFORM_WEB */
 
 #define MSG_STR "Hi, I'm going to build a snake game with raylib and wasm"
 
@@ -9,25 +14,37 @@ const int SCREEN_HEIGHT = 800;
 const char *MSG = MSG_STR;
 const size_t MSG_LEN = sizeof(MSG_STR) - 1;
 
+void UpdateDrawFrame();
+
 int main() {
 
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake babu");
-  // SetTargetFPS(60);
+
+#ifdef PLATFORM_WEB
+  // #if defined(PLATFORM_WEB)
+  printf("Hi, you selected PLATFORM_WEB\n");
+  emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+  SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
-    // Update;
-
-    // Draw;
-    BeginDrawing();
-
-    ClearBackground(RAYWHITE);
-    DrawText("Hi, I'm going to build a snake game with raylib and wasm", 170,
-             360, 20, MAGENTA);
-
-    EndDrawing();
+    UpdateDrawFrame();
   }
+#endif /* ifdef PLATFORM_WEB */
 
   CloseWindow();
-
   return 0;
+}
+
+void UpdateDrawFrame() {
+  // Update;
+
+  // Draw;
+  BeginDrawing();
+
+  ClearBackground(RAYWHITE);
+  DrawText("Hi, I'm going to build a snake game with raylib and wasm", 170, 360,
+           20, MAGENTA);
+
+  EndDrawing();
 }
