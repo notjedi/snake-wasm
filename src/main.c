823b18d09a84f583c0ab348e93f4bf405480e91b
Krithic Kumar <krithickumarub@protonmail.com>
2024-03-20 16:27:47 +0530

    basic working grid witha rectangle moving around

--- src/main.c
+++ src/main.c
@@ -8,11 +8,15 @@
 #endif /* ifdef PLATFORM_WEB */
 
 #define MSG_STR "Hi, I'm going to build a snake game with raylib and wasm"
+#define GRID_W 20.0f
+#define GRID_H 20.0f
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 800;
+const int SCREEN_WIDTH = 400;
+const int SCREEN_HEIGHT = 400;
 const char *MSG = MSG_STR;
 const size_t MSG_LEN = sizeof(MSG_STR) - 1;
+
+static Rectangle snake_head = {0.0f, 0.0f, GRID_W, GRID_H};
 
 void UpdateDrawFrame();
 
@@ -21,7 +25,6 @@ int main() {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake babu");
 
 #ifdef PLATFORM_WEB
-  printf("Hi, you selected PLATFORM_WEB\n");
   emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
   SetTargetFPS(60);
@@ -37,13 +40,42 @@ int main() {
 
 void UpdateDrawFrame() {
   // Update;
+  if (IsKeyDown(KEY_RIGHT)) {
+    snake_head.x += GRID_W;
+  } else if (IsKeyDown(KEY_LEFT)) {
+    snake_head.x -= GRID_W;
+  } else if (IsKeyDown(KEY_UP)) {
+    snake_head.y -= GRID_H;
+  } else if (IsKeyDown(KEY_DOWN)) {
+    snake_head.y += GRID_H;
+  }
+
+  if (snake_head.x > SCREEN_WIDTH)
+    snake_head.x = 0;
+  else if (snake_head.x < 0)
+    snake_head.x = SCREEN_WIDTH;
+
+  if (snake_head.y > SCREEN_HEIGHT)
+    snake_head.y = 0;
+  else if (snake_head.y < 0)
+    snake_head.y = SCREEN_HEIGHT;
 
   // Draw;
   BeginDrawing();
 
-  ClearBackground(RAYWHITE);
-  DrawText("Hi, I'm going to build a snake game with raylib and wasm", 170, 360,
-           20, MAGENTA);
+  { // BeginDrawing
+    ClearBackground(RAYWHITE);
+    for (int i = 0; i < SCREEN_WIDTH / GRID_W; i++) {
+      DrawLineV((Vector2){i * GRID_W, 0.0f},
+                (Vector2){i * GRID_W, SCREEN_HEIGHT}, LIGHTGRAY);
+      for (int j = 0; j < SCREEN_HEIGHT / GRID_H; j++) {
+        DrawLineV((Vector2){0.0f, j * GRID_H},
+                  (Vector2){SCREEN_WIDTH, j * GRID_H}, LIGHTGRAY);
+      }
+    }
+
+    DrawRectangleRec(snake_head, RED);
+  } // EndDrawing
 
   EndDrawing();
 }
